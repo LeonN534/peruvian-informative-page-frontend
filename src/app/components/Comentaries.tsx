ba1c65613b030658beb2f4bed9346024e2d08060
TechTeamCMH <techteamcmh@gmail.com>
2024-11-25 23:14:37 -0500

    Added buttons to navigate through comments

--- src/app/components/Comentaries.tsx
+++ src/app/components/Comentaries.tsx
@@ -4,6 +4,8 @@ import { getComentariesByTopic } from "../services/commentaries.service";
 import { Commentary } from "../interfaces/commentary.interface";
 import CommentaryCard from "./CommentaryCard";
 import CommentaryForm from "./CommentaryForm";
+import Link from "next/link";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 const Comentaries = async ({
   topic,
@@ -14,10 +16,18 @@ const Comentaries = async ({
 }) => {
   const resData = await getComentariesByTopic(topic, 10 * (page - 1));
 
-  if (!resData.success)
+  if (!resData.success || resData.data.length === 0)
     return (
-      <div className="bg-slate-900 text-4xl font-bold text-red-600 p-10">
-        No comments to show
+      <div className="bg-slate-900 text-4xl font-bold text-red-600 p-10 text-center">
+        Sin comentarios que mostrar
+        <div className="flex items-center justify-center gap-10">
+          <Link href={`/${topic}?page=${page - 1}`}>
+            <ArrowLeft className="w-10 h-10 mt-5 text-white" />
+          </Link>
+          <Link href={`/${topic}?page=${page + 1}`}>
+            <ArrowRight className="w-10 h-10 mt-5 text-white" />
+          </Link>
+        </div>
       </div>
     );
 
@@ -30,6 +40,14 @@ const Comentaries = async ({
         {comments.map((comment) => (
           <CommentaryCard key={comment.id} commentary={comment} />
         ))}
+      </div>
+      <div className="flex items-center justify-center gap-10">
+        <Link href={`/${topic}?page=${page - 1}`}>
+          <ArrowLeft className="w-10 h-10 mt-5" />
+        </Link>
+        <Link href={`/${topic}?page=${page + 1}`}>
+          <ArrowRight className="w-10 h-10 mt-5" />
+        </Link>
       </div>
       <CommentaryForm topic={topic} />
     </div>
